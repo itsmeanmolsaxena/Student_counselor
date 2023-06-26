04a5b6b061efed3f9ac07a955d5babc72a81fa02
itsmeanmolsaxena <itsmeanmolsaxena@gmail.com>
2023-06-27 01:17:09 +0530

    updated the data ingestion file to create csv files in artifacts folder

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,7 +20,8 @@ from src.exception import CustomException
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
-    # raw_data_path: str=os.path.join('artifacts',"data.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
     
 
 class DataIngestion:
@@ -37,14 +38,14 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            # df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            # logging.info("Train test split initiated")
-            # train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            # train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            # test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Inmgestion of the data iss completed")
 
